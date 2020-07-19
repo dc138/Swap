2a86921e32884b63099e30c67e1af0e2be4693ca
DarthChungo <aharomar2004@gmail.com>
2020-07-20 01:27:15 +0200

    Added main class.

---
+++ src/me/DarthChungo/Swap/Swap.java
@@ -0,0 +1,24 @@
+package me.DarthChungo.Swap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Swap extends JavaPlugin {
+    public SwapGame sg;
+    public SwapCommand sc;
+    public SwapEvent se;
+    public SwapTask st;
+
+    @Override
+    public void onEnable() {
+        this.sg = new SwapGame(this);
+        this.sc = new SwapCommand(this);
+        this.se = new SwapEvent(this);
+
+        getLogger().info("Swap plugin by DarthChungo enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Swap plugin by DarthChungo disabled.");
+    }
+}
